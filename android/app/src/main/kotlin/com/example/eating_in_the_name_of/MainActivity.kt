3d8b79f6b88898b0407e7426535b1592fa276c4e
Rafa3666 <rafaelperdomo1@hotmail.com>
2024-10-08 15:36:04 -0300

    Commit

---
+++ android/app/src/main/kotlin/com/example/eating_in_the_name_of/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.eating_in_the_name_of
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Let's cook?",
+      color: Colors.white,
+      home: CategoriesScreen(),
+    );
+  }
+}
